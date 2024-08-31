7cb306fb55081c69d3f29e3ba0641bccfa9d3a16
chriswiesanjaya <chriswiesanjaya@gmail.com>
2024-08-31 11:56:33 +1000

    added avg rating, added table to EventsView

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -4,15 +4,38 @@
     <div class="row">
       <div class="col-md-8 offset-md-2">
         <h1 class="text-center">Events</h1>
+
+        <!-- Events table -->
         <div class="row text-center">
-          <p></p>
+          <DataTable :value="events" tableStyle="min-width: 50rem">
+            <Column field="name" header="Name"></Column>
+            <Column field="location" header="Location"></Column>
+            <Column field="date" header="Date"></Column>
+            <Column field="donation" header="Donation"></Column>
+            <Column field="volunteer" header="Volunteer"></Column>
+            <Column field="rating" header="Rating"></Column>
+          </DataTable>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+
+const storedEvents = ref(JSON.parse(localStorage.getItem('events')) || [])
+
+// Add computed rating to each event
+const events = ref(
+  storedEvents.value.map((event) => ({
+    ...event,
+    rating: event.numberRating ? (event.totalRating / event.numberRating).toFixed(2) : 0
+  }))
+)
+</script>
 
 <style scoped>
 .container {
